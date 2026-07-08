9e21dadcd15b3c264144066d5db88288b2b43832
rednic <rednic@ltos.ro>
2026-07-08 22:34:34 +0300

    chore: copyright Rednic Vasile in LICENSE + all 26 script headers

--- scripts/parse-evtx.sh
+++ scripts/parse-evtx.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright (c) 2026 rednicv
+# Copyright (c) 2026 Rednic Vasile
 # Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
 # RedSeek Rescue - parse-evtx.sh
